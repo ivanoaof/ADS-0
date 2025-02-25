982ac267da490f5e449552228f3c5c31312318f7
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-02-25 20:52:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,20 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 
 int gcd(int a, int b) {
+	int arr[100], index = 0;
+	while (a != b) {
+		int diff = abs(a - b);
+		arr[index] = diff;
+		index++;
+		if (a > b) {
+			a = diff;
+		}
+		else {
+			b = diff;
+		}
 
+	}
+	int min_num = *min_element(arr, arr + index);
+	return min_num;
 }
+
