aa0cfd90801d9fcfebcd5d23a6013b3a22efce86
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-02-25 21:23:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,22 @@
-// Copyright 2025 NNTU-CS
-
 #include <iostream>
 #include <cmath>
 #include <algorithm>
 using namespace std;
 
 int gcd(int a, int b) {
-  int arr[100], index = 0;
-  while (a != b) {
-    int diff = abs(a - b);
-    arr[index] = diff;
-    index++;
-    if (a > b) {
-      a = diff;
-    } else {
-      b = diff;
-    }
-  }
-  int min_num = *min_element(arr, arr + index);
-  return min_num;
+	int arr[100], index = 0;
+	while (a != b) {
+		int diff = abs(a - b);
+		arr[index] = diff;
+		index++;
+		if (a > b) {
+			a = diff;
+		}
+		else {
+			b = diff;
+		}
+
+	}
+	int min_num = *min_element(arr, arr + index);
+	return min_num;
 }
