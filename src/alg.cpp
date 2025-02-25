3c495998bba0e460ca9b126b796cf341c432798a
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-02-25 21:04:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,20 +6,17 @@
 using namespace std;
 
 int gcd(int a, int b) {
-	int arr[100], index = 0;
-	while (a != b) {
-		int diff = abs(a - b);
-		arr[index] = diff;
-		index++;
-		if (a > b) {
-			a = diff;
-		}
-		else {
-			b = diff;
-		}
-
-	}
-	int min_num = *min_element(arr, arr + index);
-	return min_num;
+  int arr[100], index = 0;
+  while (a != b) {
+    int diff = abs(a - b);
+    arr[index] = diff;
+    index++;
+    if (a > b) {
+      a = diff;
+    } else {
+      b = diff;
+    }
+  }
+  int min_num = *min_element(arr, arr + index);
+  return min_num;
 }
-
